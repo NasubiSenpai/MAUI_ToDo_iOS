9f7caf91b02de19d5f3d07a5ba56d5b9a03d1457
PAISENSAN <colt.firearms.g@gmail.com>
2023-04-19 00:44:18 +0900

    Comment追加 プロパティ名変更

--- ToDoApp/Controls/Models/PopUpTextViewModel.cs
+++ ToDoApp/Controls/Models/PopUpTextViewModel.cs
@@ -11,11 +11,26 @@ namespace ToDoApp.Controls.Models
 {
     public partial class PopUpTextViewModel : ObservableObject
     {
+        /// <summary>
+        /// 変更通知プロパティ。
+        /// [NotifyPropertyChangedFor(nameof(hoge))]属性で
+        /// 指定したhogeプロパティを仕様する必要がある
+        /// *属性付きメンバ変数m_HogeHogeは直接参照不可
+        /// </summary>
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(UpperText))]
+        [NotifyPropertyChangedFor(nameof(propUpperText))]
         private string m_PopUpText = "hello, world";
 
-        public string UpperText => PopUpText.ToUpper();
+        /// <summary>
+        /// PopUPTextプロパティを大文字に変換して返す
+        /// </summary>
+        public string propUpperText => PopUpText.ToUpper();
+
+        /// <summary>
+        /// 表示されているテキストを返す
+        /// </summary>
+        /// <returns>propUpperText</returns>
+        public string GetText() => propUpperText;
 
     }
 }
