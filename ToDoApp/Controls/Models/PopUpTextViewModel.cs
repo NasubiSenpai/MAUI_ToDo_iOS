0c0f008255a6a1152a7f37e045dd7ca2d78b76fd
PAISENSAN <colt.firearms.g@gmail.com>
2023-04-19 00:34:56 +0900

    CommunityToolKitMVVM試作。未検証

---
+++ ToDoApp/Controls/Models/PopUpTextViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace ToDoApp.Controls.Models
+{
+    public partial class PopUpTextViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(UpperText))]
+        private string m_PopUpText = "hello, world";
+
+        public string UpperText => PopUpText.ToUpper();
+
+    }
+}
